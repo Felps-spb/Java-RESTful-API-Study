2cf21ac1136cf2dcd4c7ee0bb1d9f050db53fee5
Luiz Felipe <luizfelipespb22@gmail.com>
2025-02-20 15:49:16 -0300

    Initial commit

---
+++ src/main/java/teste/api/Teste_criacao_api/TesteCriacaoApiApplication.java
@@ -0,0 +1,13 @@
+package teste.api.Teste_criacao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteCriacaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteCriacaoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teste/api/Teste_criacao_api/TesteCriacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package teste.api.Teste_criacao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteCriacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
